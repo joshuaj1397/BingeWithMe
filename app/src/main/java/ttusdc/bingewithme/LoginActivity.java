51949705797a3f5f53ec161230738ff6678ebf4c
Joshua Johnson <joshuaj1397@gmail.com>
2018-01-10 19:25:19 -0600

    Updated compile version to SDK 27 and implemented login via facebook

--- app/src/main/java/ttusdc/bingewithme/LoginActivity.java
+++ app/src/main/java/ttusdc/bingewithme/LoginActivity.java
@@ -3,11 +3,45 @@ package ttusdc.bingewithme;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class LoginActivity extends AppCompatActivity {
+
+  @BindView(R.id.fb_login_button) LoginButton loginButton;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_login);
+    ButterKnife.bind(this);
+
+    CallbackManager callbackManager = CallbackManager.Factory.create();
+    loginButton.setReadPermissions("email", "public profile", "user_friends");
+    loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
+      // TODO: Implement successful login method
+      @Override
+      public void onSuccess(LoginResult loginResult) {
+        // App code
+      }
+
+      @Override
+      public void onCancel() {
+        // Do nothing
+      }
+
+      // TODO: Implement debug logging
+      @Override
+      public void onError(FacebookException exception) {
+        // App code
+      }
+    });
   }
 }
